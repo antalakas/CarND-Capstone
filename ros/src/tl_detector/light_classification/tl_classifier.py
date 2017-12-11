85f7edcf1515732b7c9d2c20f560fa4dfda72cf8
Brian <blester125@gmail.com>
2017-12-10 21:56:31 -0500

    Quick model.

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,4 +1,5 @@
 from styx_msgs.msg import TrafficLight
+import tensorflow as tf
 
 class TLClassifier(object):
     def __init__(self):
@@ -17,3 +18,59 @@ class TLClassifier(object):
         """
         #TODO implement light color prediction
         return TrafficLight.UNKNOWN
+
+def make_model():
+    inputs = tf.placeholder(tf.float32, shape=[None, None], name="inputs")
+    keep_prob = tf.placeholder(tf.float32, name="dropout")
+
+    # Convolutional
+    conv1 = tf.layers.conv2d(
+        filters=16,
+        kernel_size=5,
+        padding="SAME"
+    )
+    pool1 = tf.layers.max_pooling2d(
+        conv1,
+        pool_size=2,
+        strides=2
+    )
+    out1 = tf.nn.relu(pool1)
+
+    conv2 = tf.layers.conv2d(
+        filters=32,
+        kernel_size=3,
+        padding="SAME"
+    )
+    pool2 = tf.layers.max_pooling2d(
+        conv2,
+        pool_size=2,
+        strides=2
+    )
+    out = tf.nn.relu(pool2)
+
+    # Global average pooling
+    pooled = tf.reduce_mean(out, axis=[1, 2])
+
+    # Fully Connected
+    fc1 = tf.layers.dense(
+        pooled,
+        200,
+        activation=tf.nn.relu
+    )
+
+    dropped1 = tf.nn.dropout(fc1, keep_prob=keep_porb)
+
+    fc2 = tf.layers.dense(
+        dropped1,
+        100,
+        activation=tf.nn.relu
+    )
+
+    logits = tf.lauers.dense(
+        fc2,
+        output_size
+    )
+
+    predictions = tf.argmax(logits, name="predictions")
+
+    return inputs, keep_prob, logits, predictions
